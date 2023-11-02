4211645a608919a9e5f8f35778b1050094559927
Johnny Stene <johnny@stene.xyz>
2023-11-01 23:46:13 -0600

    v0.2-a

--- bigdata.py
+++ bigdata.py
@@ -3,44 +3,66 @@
 # Quick n' easy data parser
 
 import os, sys, json
-global current_dataset_path, current_dataset
+global current_dataset_path, current_dataset, strict_matching
+strict_matching = False
 current_dataset_path = ""
 current_dataset = []
 
 def save_dataset():
 	try:
 		with open(current_dataset_path, "w") as outfile:
-			outfile.write(current_dataset)
-	except:
+			outfile.write(json.dumps(current_dataset))
+	except Exception as e:
 		print("Error: Couldn't write to file \"" + current_dataset_path + "\"")
+		print(e)
 
 def command():
-	global current_dataset, current_dataset_path
+	global current_dataset, current_dataset_path, strict_matching
 	raw_command = input(current_dataset_path + "> ")
 	command = raw_command.split(" ")[0]
-	args = raw_command.split(" ")[1:]
+	raw_args = raw_command.split(" ")[1:]
 
+	# Parse arguments
+	args = []
+	long_arg = ""
+	in_long_arg = False
+	for arg in raw_args:
+		if(arg.startswith("\"")):
+			in_long_arg = True
+			arg = arg[1:]
+			long_arg = arg
+		elif(in_long_arg):
+			long_arg += " " + arg
+			if(arg.endswith("\"")):
+				long_arg = long_arg[:-1]
+				args.append(long_arg)
+		else:
+			args.append(arg)
+
+	# Command handlers
 	if(command == "about"):
 		print("stene.xyz BigData")
-		print("Version 0.1-a")
+		print("Version 0.2-a")
 		print("Written by Johnny Stene <johnny@stene.xyz>")
 	elif(command == "help"):
-		print("about - About BigData");
+		print("about - About BigData")
 		print("help - Displays this menu")
 		print("")
 		print("FILE OPERATIONS")
 		print("create <file> - Create a new dataset.")
+		print("open <file> - Load a dataset.")
 		print("save - Save the current dataset.")
 		print("copy <file> - Save a copy of the current dataset.")
 		print("merge <file> - Merge this dataset with another.")
 		print("import <file> - Import a .csv file.")
 		print("export_dataset <file> - Exports the whole dataset.")
 		print("export_field <field> <file> - Exports a list of all points in the given field as a plain text file.")
+		print("info - Print database information.")
 
 		print("DATA MANIPULATION")
+		print("strict_matching - Toggle strict matching")
 		print("list_fields - List all fields in the current dataset.")
 		print("search <field> <value> - Searches for all entries that contain given value in the given field.")
-		print("search_strict <field> <value> - Searches for all entries that 100% match the given value in the given field.")
 		print("filter_include <field> <value> - Filters all data points (retain all data points where the given value is stored in the field)")
 		print("filter_exclude <field> <value> - Filters all data points (retain all data points where the given value isn't stored in the field)")
 
@@ -48,6 +70,14 @@ def command():
 		current_dataset_path = args[0] + ".bdata"
 		current_dataset = []
 		save_dataset()
+	elif(command == "open"):
+		try:
+			current_dataset_path = args[0] + ".bdata"
+			with open(current_dataset_path) as jsonfile:
+				current_dataset = json.load(jsonfile)
+		except Exception as e:
+			print("Error opening.")
+			print(e)
 	elif(command == "save"):
 		save_dataset()
 	elif(command == "copy"):
@@ -85,22 +115,23 @@ def command():
 				item_count = 0
 				for point in content.split(","):
 					if not(template[current_position] == "[IGNORE]"):
-						current_item[template[current_position]] = point
+						current_item[template[current_position]] = point.strip()
 					current_position += 1
 					if(current_position == len(template)):
 						current_position = 0
-						dataset.append(current_item)
+						current_dataset.append(current_item)
 						current_item = {}
 						item_count += 1
 				print(str(item_count) + " items imported.")
-		except:
+		except Exception as e:
 			print("Error while importing.")
+			print(e)
 	elif(command == "export_dataset"):
 		if(len(current_dataset) == 0):
 			print("Error: Current dataset empty.")
 		else:
 			try:
-				with open(args[0]) as outfile:
+				with open(args[0], "w") as outfile:
 					header = ""
 					fields = []
 					for item in current_dataset[0]:
@@ -113,8 +144,9 @@ def command():
 							outfile.write(item[fields[i]] + ",")
 						outfile.write(item[fields[-1]] + "\n")
 				print("Done.")
-			except:
+			except Exception as e:
 				print("Failed to write.")
+				print(e)
 	elif(command == "export_field"):
 		if(len(current_dataset) == 0):
 			print("Error: Current dataset empty.")
@@ -124,9 +156,29 @@ def command():
 					for item in current_dataset:
 						outfile.write(item[args[0]] + "\n")
 				print("Done.")
-			except:
+			except Exception as e:
 				print("Failed to write.")
+				print(e)
+	elif(command == "info"):
+		print("DATASET INFO:")
+		if(len(current_dataset) > 0):
+			print(str(len(current_dataset)) + " items in dataset")
+			print("Dataset has the following fields:")
+			fields = ""
+			for item in current_dataset[0]:
+				fields += item + ", "
+			fields = fields[:-1]
+			print(fields)
+		else:
+			print("Dataset is empty")
 
+	elif(command == "strict_matching"):
+		if(strict_matching):
+			print("Strict matching disabled.")
+			strict_matching = False
+		else:
+			print("Strict matching enabled.")
+			strict_matching = True
 	elif(command == "list_fields"):
 		if(len(current_dataset) == 0):
 			print("No fields.")
@@ -145,46 +197,49 @@ def command():
 				field_text += item + " | "
 			print(field_text)
 			for item in current_dataset:
-				if(args[1] in item[args[0]]):
-					results += 0
-					item_text = ""
-					for field in fields:
-						item_text += item[field] + " | "
-					print(item_text)
-	elif(command == "search_strict"):
-		if(len(current_dataset) == 0):
-			print("No items to search!")
-		else:
-			results = 0
-			fields = []
-			field_text = ""
-			for item in current_dataset[0]:
-				fields.append(item)
-				field_text += item + " | "
-			print(field_text)
-			for item in current_dataset:
-				if(args[1] == item[args[0]]):
+				match = True
+				if(strict_matching):
+					match = (args[1] == item[args[0]])
+				else:
+					match = (args[1] in item[args[0]])
+				if(match):
 					results += 0
 					item_text = ""
 					for field in fields:
 						item_text += item[field] + " | "
 					print(item_text)
 	elif(command == "filter_include"):
+		dropped = 0
 		if(len(current_dataset) > 0):
 			updated_dataset = []
 			for item in current_dataset:
-				if(args[1] == item[args[0]]):
+				match = True
+				if(strict_matching):
+					match = (args[1] == item[args[0]])
+				else:
+					match = (args[1] in item[args[0]])
+				if(match):
 					updated_dataset.append(item)
+				else:
+					dropped += 1
 			current_dataset = updated_dataset
-		print("Done.")
+		print("Done. Dropped " + str(dropped) + " items.")
 	elif(command == "filter_exclude"):
+		dropped = 0
 		if(len(current_dataset) > 0):
 			updated_dataset = []
 			for item in current_dataset:
-				if not(args[1] == item[args[0]]):
+				match = True
+				if(strict_matching):
+					match = (args[1] == item[args[0]])
+				else:
+					match = (args[1] in item[args[0]])
+				if not(match):
 					updated_dataset.append(item)
+				else:
+					dropped += 1
 			current_dataset = updated_dataset
-		print("Done.")
+		print("Done. Dropped " + str(dropped) + " items.")
 if(__name__ == "__main__"):
 	while True:
 		command()
---
+++ readme.txt
@@ -0,0 +1,21 @@
+#####################
+# stene.xyz BigData #
+#####################
+
+== About ==
+stene.xyz BigData is a small script to handle (mostly manual) parsing of CSV files.
+
+== License ==
+stene.xyz BigData is licensed CC-BY.
+
+== Changelog ==
+0.2-a:
+    - Fixed crashing bug when importing file.
+    - Better error messages.
+    - Added "info" command.
+    - Added "strict_matching" command.
+0.1-a:
+    - Initial release.
+
+== Known bugs ==
+- Parsing errors can occur if one line has a value containing commas (eg. within quotes)
